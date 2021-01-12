db28af74cc0a0e3677aeef8937dea3b653fd4afb
shakil <mahamodul.shakil11@gmail.com>
2021-01-12 19:23:30 +0600

    user authentication and profile management done

--- My_Blog_Project/settings.py
+++ My_Blog_Project/settings.py
@@ -83,10 +83,21 @@ WSGI_APPLICATION = 'My_Blog_Project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'blogwithdjango',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
